140406cc4040201d225fa8cae321caa146b5f932
Phil <phil@PhildeMac-mini.local>
2021-05-13 17:14:45 +0800

    Update demo.

---
+++ demo/Pods/IRRadioButton-swift/IRRadioButton-swift/Classes/Utilities/Utilities.swift
@@ -0,0 +1,15 @@
+//
+//  Utilities.swift
+//  IRRadioButton-swift
+//
+//  Created by Phil on 2021/5/12.
+//
+
+import Foundation
+
+class Utilities {
+    class func getCurrentBundle() -> Bundle {
+        return Bundle.init(for: self)
+    }
+}
+
